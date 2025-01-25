daaeea11465911e6734be22a8872df71ca04dfb4
Gustavo <gustavoo.sec@gmail.com>
2025-01-25 16:41:44 -0300

    anotations about type variable and yours variants in memorysize

---
+++ README.md
@@ -0,0 +1,4 @@
+# Golang Anotations
+
+- all variables which are declared in a function must be used, not exist dead code.
+- 
--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import (
-	"myFirstGoProject/function"
-)
+// bool // true or false
+// int int8 int16 int32 int64
+// uint uint8 uint16 uint32 uint64 uintptr
+// 8 bit = 1 byte
+// byte // alias for uint8 // represents the set of all unsigned 8-bit integers
+// rune // alias for int32 // represents a Unicode code point
+// float32 float64 // 32-bit and 64-bit floating-point numbers
+// complex64 complex128 // 32-bit and 64-bit complex numbers
+
+// x := 10084
+// s := strconv.FormatInt(int64(x), 10)
+// println(s)
 
 func main() {
-	println(function.Split(10, 2))
+
 }
