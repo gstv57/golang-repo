cbd7070d744885c05c0609a91b4333701f6e607d
Gustavo <gustavoo.sec@gmail.com>
2025-01-27 19:58:18 -0300

    redo guessing game

--- main.go
+++ main.go
@@ -10,42 +10,45 @@ import (
 )
 
 func main() {
-	fmt.Println("Game of Guessing")
-	fmt.Println("One number random be sorted from 1 to 100. Try to guess it. You have 10 chances.")
+	fmt.Printf("Jogo da adivinhação\n")
+	fmt.Printf("Escolha um número entre 1 e 100\n" +
+		"Você tem 10 tentativas\n")
 
 	scanner := bufio.NewScanner(os.Stdin)
-	attemps := [10]int64{}
-	x := rand.Int64N(101)
-	for i := range attemps {
-		fmt.Println("Enter your guess: ")
 
+	attemps := [10]int64{}
+
+	x := rand.Int64N(100) + 1
+
+	for i := range attemps {
+		fmt.Println("Digite um número:")
 		scanner.Scan()
 		attemp := scanner.Text()
-
 		attemp = strings.TrimSpace(attemp)
 
 		attempInteger, err := strconv.ParseInt(attemp, 10, 64)
 
 		if err != nil {
-			fmt.Println("Invalid input. Please enter a number.")
+			fmt.Println("Número inválido")
 			return
 		}
 
 		switch {
 		case attempInteger < x:
-			fmt.Println("Your guess is too low.", attempInteger)
+			fmt.Println("O número é maior")
 		case attempInteger > x:
-			fmt.Println("Your guess is too high.", attempInteger)
+			fmt.Println("O número é menor")
 		case attempInteger == x:
-			fmt.Printf("Congratulations! You guessed the number %d\n"+
-				"You guessed the number in %d attempts.\n"+
-				"These were your attempts: %v\n", x, i+1, attemps[:i])
+			fmt.Printf("Parabéns, você acertou! O número é %d \n"+
+				"Você acertou em %d tentativas \n"+
+				"Aqui está suas tentativas: %v \n", x, i+1, attemps[:i])
 			return
 		}
 
 		attemps[i] = attempInteger
 	}
 
-	fmt.Printf("You didn't guess the number. The number was %d\n", x)
-	fmt.Println("Your guesses were: ", attemps)
+	fmt.Printf("Você perdeu! O número era %d\n", x)
+	fmt.Println("Tentativas: ", attemps)
+
 }
